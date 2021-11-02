0bd0f50ef213399443fbc57e59425b11ead219bf
SeanErn <81441231+SeanErn@users.noreply.github.com>
2021-11-02 16:13:04 -0400

    matched code to robot

--- Experimental & in progress/DebugRobot.java
+++ Experimental & in progress/DebugRobot.java
@@ -12,29 +12,32 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 
 @Autonomous
 public class DebugRobot extends LinearOpMode {
-private DcMotorEx leftdrive = null;
-private DcMotorEx rightdrive = null;
-private DcMotorEx spinner = null;
+private DcMotor leftdrive = null;
+private DcMotor rightdrive = null;
+private DcMotor spinner = null;
 private Servo claw = null;
-private DcMotorEx duck = null;
-private DcMotorEx Arm = null;
+private DcMotor duck = null;
+private DcMotor Arm = null;
 private TouchSensor armBack = null;
-private DistanceSensor distance = null;
+//private DistanceSensor distance = null;
     
     @Override
     public void runOpMode() {
         DcMotorEx leftdrive  = hardwareMap.get(DcMotorEx.class, "leftdrive");
         DcMotorEx rightdrive = hardwareMap.get(DcMotorEx.class, "rightdrive");
-        DcMotorEX spinner = hardwareMap.get(DcMotor.class, "spinner");
-        DcMotorEX duck = hardwareMap.get(DcMotor.class, "duck");
-        DcMotorEx Arm = hardwareMap.get(DcMotor.class, "Arm");
+        DcMotorEx spinner = hardwareMap.get(DcMotorEx.class, "spinner");
+        DcMotorEx duck = hardwareMap.get(DcMotorEx.class, "duck");
+        DcMotorEx Arm = hardwareMap.get(DcMotorEx.class, "Arm");
         armBack = hardwareMap.get(TouchSensor.class, "armBack");
-        distance = hardwareMap.get(DistanceSensor.class, "distance");
+        //distance = hardwareMap.get(DistanceSensor.class, "distance");
         claw = hardwareMap.servo.get("claw");
 
         // Reset the encoder during initialization
         leftdrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         rightdrive.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        spinner.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        duck.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        Arm.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         waitForStart();
         
         // Set the motor's target position to 300 ticks
@@ -42,7 +45,6 @@ private DistanceSensor distance = null;
         
         // Switch to RUN_TO_POSITION mode
         leftdrive.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        rightdrive.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
         // Start the motor moving by setting the max velocity to 200 ticks per second
         leftdrive.setVelocity(200);
         
@@ -52,16 +54,19 @@ telemetry.addData("Status", "Waiting for left drive to test");
 telemetry.addData("Left drive position", leftdrive.getCurrentPosition());
  telemetry.update();
 }
-if(leftdrive.getCurrentPosition() = 300){
-telemetry.addData("Left Drive" , "PASS");
+if(leftdrive.getCurrentPosition() >= 300){
+ telemetry.addLine()
+.addData("Left Drive" , "PASS");
 telemetry.update();
 } else {
-telemetry.addData("Right Drive" , "FAIL");
+telemetry.addLine()
+.addData("Right Drive" , "FAIL");
 telemetry.update();    
 }
 
 //test right drive
 rightdrive.setTargetPosition(-300);
+rightdrive.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 rightdrive.setVelocity(200);
 
 while(rightdrive.isBusy()) {
@@ -69,16 +74,19 @@ telemetry.addData("Right drive position", rightdrive.getCurrentPosition());
 telemetry.addData("Status", "Waiting for right drive to test");
 telemetry.update();
 }
- if(rightdrive.getCurrentPosition() = -300){
-telemetry.addData("Right Drive" , "PASS");
+ if(rightdrive.getCurrentPosition() >= -300){
+telemetry.addLine()
+.addData("Right Drive" , "PASS");
 telemetry.update();
 } else {
-telemetry.addData("Right Drive" , "FAIL");
+telemetry.addLine()
+.addData("Right Drive" , "FAIL");
 telemetry.update();    
 }
 
 //test duck
 duck.setTargetPosition(-300);
+duck.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 duck.setVelocity(200);
 
 while(duck.isBusy()) {
@@ -86,16 +94,19 @@ telemetry.addData("Duck Spinner position", duck.getCurrentPosition());
 telemetry.addData("Status", "Waiting for duck spinner to test");
 telemetry.update();
 }
- if(duck.getCurrentPosition() = -300){
-telemetry.addData("Duck Spinner" , "PASS");
+ if(duck.getCurrentPosition() >= -300){
+ telemetry.addLine()
+.addData("Duck Spinner" , "PASS");
 telemetry.update();
 } else {
-telemetry.addData("Duck Spinner" , "FAIL");
+telemetry.addLine()
+.addData("Duck Spinner" , "FAIL");
 telemetry.update();    
 }
 
 //test spinner
 spinner.setTargetPosition(300);
+spinner.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 spinner.setVelocity(200);
 
 while(spinner.isBusy()) {
@@ -103,30 +114,18 @@ telemetry.addData("Arm Spinner position", spinner.getCurrentPosition());
 telemetry.addData("Status", "Waiting for arm spinner to test");
 telemetry.update();
 }
- if(spinner.getCurrentPosition() = 300){
-telemetry.addData("Arm Spinner" , "PASS");
+ if(spinner.getCurrentPosition() >= 300){
+telemetry.addLine()
+.addData("Arm Spinner" , "PASS");
 telemetry.update();
 } else {
-telemetry.addData("Arm Spinner" , "FAIL");
+telemetry.addLine()
+.addData("Arm Spinner" , "FAIL");
 telemetry.update();    
 }
 
 //test arm
-Arm.setTargetPosition(300);
-Arm.setVelocity(200);
-
-while(Arm.isBusy()) {
-telemetry.addData("Arm position", Arm.getCurrentPosition());
-telemetry.addData("Status", "Waiting for arm to test");
-telemetry.update();
-}
- if(Arm.getCurrentPosition() = 300){
-telemetry.addData("Arm" , "PASS");
-telemetry.update();
-} else {
-telemetry.addData("Arm" , "FAIL");
-telemetry.update();    
-}
+//arm doesn't have encoder
 
 //test push button
 Arm.setTargetPosition(0);
@@ -137,17 +136,20 @@ telemetry.addData("Arm position", Arm.getCurrentPosition());
 telemetry.addData("Status", "Waiting for arm to retract then proceeding");
 telemetry.update();
 }
- if(Arm.getCurrentPosition() = 0  , armBack.isPressed()){
-telemetry.addData("Arm location sensor" , "PASS");
+ if(armBack.isPressed()){
+telemetry.addLine()
+.addData("Arm location sensor" , "PASS");
 telemetry.update();
 } else {
-telemetry.addData("Arm location sensor" , "FAIL");
+telemetry.addLine()
+.addData("Arm location sensor" , "FAIL");
 telemetry.update();    
 }
 
 //test distance sensor
-telemetry.addData("Distance sensor and Claw servo must be tested manually" , "Switch to a Tele-Op mode to test");
-
+telemetry.addLine()
+.addData("Distance sensor, Claw servo and Arm must be tested manually" , "Switch to a Tele-Op mode to test");
+telemetry.update();
 
         // While the Op Mode is running, show the motor's status via telemetry
         while (opModeIsActive()) {
