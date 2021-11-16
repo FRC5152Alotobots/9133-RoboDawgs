639e4c4a51e80cbdfa529ed43f0a351f5bf9ef27
SeanErn <81441231+SeanErn@users.noreply.github.com>
2021-11-15 20:10:42 -0500

    added dif folders for code and made backups. Also started transfering repeating codes to dif classes

---
+++ Experimental & in progress/11.15.21-EX/DmapAuto.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.LED;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import java.util.List;
+
+public class DmapAuto {
+    /* Public OpMode members. */
+     public DcMotorEx leftdrive = null;
+    public DcMotorEx rightdrive = null;
+    public DcMotorEx spinner = null;
+    public Servo claw = null;
+    public DcMotorEx duck = null;
+    public DcMotorEx Arm;
+    public TouchSensor armBack; 
+    public Servo hook = null;
+    public RevBlinkinLedDriver led;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public DmapAuto(){
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftdrive  = hardwareMap.get(DcMotorEx.class, "leftdrive");
+        rightdrive = hardwareMap.get(DcMotorEx.class, "rightdrive");
+        spinner = hardwareMap.get(DcMotorEx.class, "spinner");
+        claw = hardwareMap.servo.get("claw");
+        duck = hardwareMap.get(DcMotorEx.class, "duck");
+        Arm = hardwareMap.get(DcMotorEx.class, "Arm");
+        armBack = hardwareMap.get(TouchSensor.class, "armBack");
+        led = hardwareMap.get(RevBlinkinLedDriver.class , "led");
+    }
+
+    //reset all encoders
+    public void resetEncoders() {
+    
+    }
+ }
