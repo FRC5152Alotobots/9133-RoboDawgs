9eddce1dc8674f062de81e7900c2213deef58cf2
Rkortlandt <93385295+Rkortlandt@users.noreply.github.com>
2021-11-29 16:54:54 -0500

    added broken code to fix

---
+++ AutoTFODblueB-EX-NOTWORKING.java
@@ -0,0 +1,190 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.List;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.LED;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@Autonomous(name = "AutoTFODblueB")
+//@Disabled
+public class AutoTFODblueB extends LinearOpMode {
+
+//makes a copy of the code in the file DmapAuto to run in this file
+DmapAutoBlue ahw = new DmapAutoBlue(); 
+//local vars
+boolean isDuckDetected = false;
+float dpos = -1;
+boolean dtc = false;
+    private static final String TFOD_MODEL_ASSET = "team-marker.tflite";
+    private static final String[] LABELS = {
+    "Marker"
+    };
+    
+    private static final String VUFORIA_KEY =
+            "AcDUHgP/////AAABmUftIp+4wU0MjlXQTngOYdWBkgSPf0WeR0r5SpGlcLSTNSTvFABVgKGwtzwk3kvp+OoKsCB5dy6ghVPMdJtbt8sqN8qsRMooX5VznbUd5ttKF2LWbx8cEUeJCO5a/XObDRvP1j5Rsbd/nHSQfdHboE65uQEtOt2Qi1X34+LA4QQDi3bdVHDN312zEGjhj7Sj4XunrgAMfQZ3Da6fNuOlJbDAgAB9BTPn8UMd5oR1SW9LbMuQZQuc2bhueMhN+ch+7LXaKuDmsXYGfQuWOpVVF4SMUBRL+QTA5RwDVgML6dKhQCHfbY+ffFIHV29K4FV77S3vE3w8ThUn0I8guItzUWWayo1GTCSs6zc153MrEB+u";
+
+    /**
+     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
+     * localization engine.
+     */
+    private VuforiaLocalizer vuforia;
+
+    /**
+     * {@link #tfod} is the variable we will use to store our instance of the TensorFlow Object
+     * Detection engine.
+     */
+    private TFObjectDetector tfod;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        //init devices from a file called DmapAuto using the copy made earlier
+    ahw.init(hardwareMap);
+
+        // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
+        // first.
+        initVuforia();
+        initTfod();
+
+        /**
+         * Activate TensorFlow Object Detection before we wait for the start command.
+         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
+         **/
+        if (tfod != null) {
+            tfod.activate();
+        }
+
+        /** Wait for the game to begin */
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.addData("Motor Status:", "Reset Encoders");
+        ahw.resetEncoders();
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
+                if (tfod != null) {
+                    // getUpdatedRecognitions() will return null if no new information is available since
+                    // the last time that call was made.
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                      telemetry.addData("# Object Detected", updatedRecognitions.size());
+                      // step through the list of recognitions and display boundary info.
+                      int i = 0;
+                      for (Recognition recognition : updatedRecognitions) {
+                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                recognition.getLeft(), recognition.getTop());
+                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                recognition.getRight(), recognition.getBottom());
+                        i++;
+                        
+                            // check label to see if the camera now sees a Duck
+                        if (recognition.getLabel().equals("Marker")) {            
+                             isDuckDetected = true;                             
+                             telemetry.addData("Object Detected", "Marker");
+                             ahw.led.setPattern(RevBlinkinLedDriver.BlinkinPattern.GREEN);
+                         } else {                                               
+                             isDuckDetected = false;  
+                            ahw.led.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+                         }
+
+
+                         //check position from the left side of the screen for the duck
+                         dpos = recognition.getLeft();
+                         if (dtc == false){
+                        if (dpos == -1){
+                        telemetry.addData("1 OR 4 DICE ROLL" , "(LEFT)");
+                        telemetry.addData("Running class:" , "BlueBlow");
+                        telemetry.update();
+                        sleep(1000);
+                        dtc = true;
+                        vuforia.setFrameQueueCapacity(0);
+                        tfod.shutdown();
+                        //add a dif class that runs dif code
+                            }
+                        if (dpos < 349 && dpos > 0){
+                        telemetry.addData("2 OR 5 DICE ROLL" , "(MIDDLE)");
+                        telemetry.addData("Running class:" , "BlueBmid");
+                        telemetry.update();
+                        sleep(1000);
+                        dtc = true;
+                        vuforia.setFrameQueueCapacity(0);
+                        tfod.shutdown();
+                        
+                        ahw.BlueBmid();
+                        //add a dif class that runs dif code
+                        }
+                        if (dpos < 550  && dpos > 350){
+                        telemetry.addData("3 OR 6 DICE ROLL" , "(RIGHT)");
+                        telemetry.addData("Running class:" , "BlueBhigh");
+                        telemetry.update();
+                        sleep(1000);
+                        dtc = true;
+                        vuforia.setFrameQueueCapacity(0);
+                        tfod.shutdown();
+                        //add a dif class that runs dif code
+                        }
+                         }
+                      }
+                      telemetry.update();
+                      
+                    }
+                }
+            }
+        }           
+    }
+
+    /**
+     * Initialize the Vuforia localization engine.
+     */
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+            "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+       tfodParameters.minResultConfidence = 0.55f;
+       tfodParameters.isModelTensorFlow2 = true;
+       tfodParameters.inputSize = 320;
+       tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+       tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
+    }
+    
+}
+
+/*
+error code: webcam not eccesable failed to init vuforia.
+ */
